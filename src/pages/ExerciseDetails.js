bdb25296f5b3b359436e92c3b0b030322739126c
Mohammed Abdulai <nurud43@gmail.com>
2025-12-05 20:02:37 -0500

    implement Exercise details

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box, Stack, Typography } from '@mui/material'
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetails = () => {
   return (
-    <div>
-      ExerciseDetails
-    </div>
+    <Box>
+      <Detail/>
+      <ExerciseVideos/>
+      <SimilarExercises/>
+    </Box>
   )
 }
 
